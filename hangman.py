cdd859daaf301b21b83026e4bd0a0f486847925e
izzygren22 <igrenier22@penguinhall.org>
2021-09-21 13:36:43 -0400

    class 2

--- hangman.py
+++ hangman.py
@@ -1,34 +1,56 @@
 # Hangman Game
+    
+wordList = ['cherry', 'apple', 'watermelon']
+gameEnd = False
+word = [ 'a', 'p', 'p', 'l', 'e']
+guess = input('Enter Guess: ')
+guessNum = 10;
 
+def getHdnWord(word):
+    hdnWord = []
+    for x in word: 
+        hdnWord.append('*')
+    #print(' '.join(hdnWord)) #print hidden word as string
+    return hdnWord
+
+    
+
+def checkGuess(word, guess, guessNum):
+    guessNum -= 1
+    if guess in word: # if guess correct
+        print('Correct!')
+        guess
+        changeWord(getHdnWord(word), guess, word)
+    guesstxt = 'You have {} guesses left!'
+    print(guesstxt.format(guessNum))
+    
+    
+
+
+def changeWord(hdnWord, guess, word):
+    for i in range(len(word)):
+        if word[i] == guess:
+            hdnWord[i] = guess
+    print(' '.join(hdnWord))
+            
+#def runGame():
+   # while gameEnd == False:
+        ##startGame();
+        #checkGuess(word,guess,guessNum);
+    ##EndMessage();
+    
+
+    
+    
+checkGuess(word, guess, guessNum)
 #def runGame():
     #while gameEnd = False:
         #startGame();
         #checkGuess();
     #EndMessage();
+
+# TO DO:
+    #check answer()
+    #choose word()
+    #end message()
     
-wordList = ['cherry', 'apple', 'watermelon']
-guesses = 10
-gameEnd = False
-word = [ 'a', 'p', 'p', 'l', 'e']
-guess = input('Enter Guess: ')
-hdnWord = []
-
-def getHdnWord():
-    for x in word:
-        hdnWord.append('*')
-    print(hdnWord)
-    
-
-def checkGuess():
-    guesses -= 1
-    if guess in word:
-        print('Correct')
-
-
-def changeWord(word, guess):
-    for guess in word:
-        pass
-        
-getHdnWord()
-#checkGuess()
-#changeWord()
